62fec067f2ff29b174c42d817e09d28fb03b1dc1
Jack Eakle <jackeakle@gmail.com>
2024-02-28 14:54:28 -0500

    Added signup context to navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,12 +1,29 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/userLogut'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
+    const { logout } = useLogout()
+    const { user } = useAuthContext()
+
+    const handleClick = () => {
+        logout()
+    }
+
+
     return (
         <header>
             <div className="container">
                 <Link to="/">
                     <h1>PawfectMatch</h1>
                 </Link>
+                <nav>
+                    {user && (
+                        <div>
+                            <Link to="/signup">Signup</Link>
+                        </div>
+                    )}
+                </nav>
             </div>
         </header>
     )
