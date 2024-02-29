55bfe020ca5206850aec773ebf8133df07b817fd
Jack Eakle <41701236+jaeakle@users.noreply.github.com>
2024-02-29 12:32:41 -0500

    Update AuthContext.js
    Typo :(

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -13,7 +13,7 @@ export const authReducer = (state, action) => {
     }
 }
 
-export const AuthConextProvider = ({ children }) => {
+export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
