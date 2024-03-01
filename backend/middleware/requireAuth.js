ee37b93f25457562d437dfa47e87d52beb9c561c
Jack Eakle <jackeakle@gmail.com>
2024-03-01 10:28:07 -0500

    Finished up Authentication, fixed spelling errors.

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 const requireAuth = async (req, res, next) => {
-    const { auhtorization } = req.headers
+    const { authorization } = req.headers
 
-    if(!auhtorization) {
-        return res.status(401).json({error: 'Authoriation token required '})
+    if(!authorization) {
+        return res.status(401).json({error: 'Authorization token required'})
     }
 
     const token = authorization.split(' ')[1]
