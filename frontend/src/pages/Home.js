31eeddcd8eeef5ee11aca8dcd76b3ba255fd94ab
Ryan Lewis <ryanlewis0117@gmail.com>
2024-03-08 00:05:46 -0500

    Log in page progess

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,17 +3,19 @@ import '../styles/home.css'
 
  const Home = () => { 
   return (
-    <div className='home-container'>
-        <img className='logo-container' src={'/images/logo.png'} alt="Pawfect Match"/>
-        <div className='home-button-container'>
-            <Link to="/login">
-                <button className='home-button'>Login</button>
-            </Link>
-            <Link to="/signup">
-                <button className='home-button'>Sign Up</button>
-            </Link>
+    <body className='home-body'>
+        <div className='home-container'>
+            <img className='logo-container' src={'/images/logo.png'} alt="Pawfect Match"/>
+            <div className='home-button-container'>
+                <Link to="/login">
+                    <button className='home-button'>Login</button>
+                </Link>
+                <Link to="/signup">
+                    <button className='home-button'>Sign Up</button>
+                </Link>
+            </div>
         </div>
-    </div>
+    </body>
   )
  }
 
