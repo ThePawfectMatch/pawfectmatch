b76f74c52d28166676703c2d445d1b32d6d61138
Ryan Lewis <100620085+Ryan17Lewis@users.noreply.github.com>
2024-03-18 18:01:24 -0400

    Feature/pm 33 (#18)
    * secret
    
    * loading fix
    
    * x
    
    * merge conflict resolved

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,49 @@
 import { Link } from 'react-router-dom'
 import '../styles/home.css'
+import React, { useState } from 'react'
 
  const Home = () => { 
+
+    const [isActive, setIsActive] = useState(false);
+    const [isReversed, setIsReversed] = useState(false);
+    const [isChanging, setIsChanging] = useState(false);
+
+    const handleAnimation = () => {
+        if (!isChanging) {
+            setIsChanging(true);
+            setIsActive(!isActive);
+            setIsReversed(false);
+          setTimeout(() => {
+            setIsChanging(false);
+          }, 300);
+        }
+    };
+
+    const handleReverseAnimation = () => {
+        if (!isChanging) {
+            setIsChanging(true);
+            setIsActive(!isActive);
+            setIsReversed(true);
+          setTimeout(() => {
+            setIsChanging(false);
+          }, 300);
+        }
+    };
+
   return (
     <body className='home-body'>
         <div className='home-container'>
-            <img className='logo-container' src={'/images/logo.png'} alt="Pawfect Match"/>
+            <img className='logo-container' onClick={isActive ? handleReverseAnimation : handleAnimation} src={'/images/logo.png'} alt="Pawfect Match"/>
             <div className='home-button-container'>
                 <Link to="/login">
-                    <button className='home-button'>Login</button>
+                    <button className='home-button' >Login</button>
                 </Link>
                 <Link to="/signup">
-                    <button className='home-button'>Sign Up</button>
+                    <button className='home-button' >Sign Up</button>
                 </Link>
             </div>
         </div>
+        <img className={`slide-dog ${isActive ? 'active' : ''} ${isReversed ? 'reverse' : ''}`} src={'/images/puppy_peeking1.png'} alt="puppy_peeking1"/>
     </body>
   )
  }
