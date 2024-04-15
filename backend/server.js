24d14e1a843c495f75107182a073e372e79021c1
Jack Eakle <jackeakle@gmail.com>
2024-04-15 08:44:39 -0400

    finalized algo

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const listingRoutes = require('./routes/listings')
 const userRoutes = require('./routes/user')
 const uploadRoutes = require('./routes/upload')
 const openAIRoutes = require('./routes/openai')
+const compatibilityRoutes = require('./routes/compatibility')
 
 // express app
 const app = express()
@@ -23,6 +24,7 @@ app.use('/api/listings', listingRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/upload', uploadRoutes)
 app.use('/api/openai', openAIRoutes)
+app.use('/api/compatibility', compatibilityRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
